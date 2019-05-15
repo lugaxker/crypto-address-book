d3b5b7238db3c6d655b9612bb2477b3c8f448569
Ludovic Lars <lug.axker@gmail.com>
2019-05-15 08:34:51 +0000

    Added main python file

---
+++ cab.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+
+  Create a cryptocurrency address book using this script.
+  
+  This script is highly experimental. Do not use it to store large amounts.
+
+'''
+
+if __name__ == '__main__':
+    pass
